b76d9da07803e0dab4367d14f9fd7a5cbe40a87b
岡優志 <yu.j.0513@gmail.com>
2021-05-19 11:11:14 +0900

    first commit

---
+++ MaterialForSwiftBasicPractice/MaterialForSwiftBasicPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MaterialForSwiftBasicPractice
+//
+//  Created by 岡優志 on 2021/05/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MaterialForSwiftBasicPractice/MaterialForSwiftBasicPractice/MaterialForSwiftBasicPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialForSwiftBasicPracticeApp.swift
+//  MaterialForSwiftBasicPractice
+//
+//  Created by 岡優志 on 2021/05/19.
+//
+
+import SwiftUI
+
+@main
+struct MaterialForSwiftBasicPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
